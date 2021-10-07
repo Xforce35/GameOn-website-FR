b68eac3a0df88c55634f17e2f6a750f327abfad0
xforce35 <xforce35@hotmail.fr>
2021-10-07 11:48:44 +0200

    remise en etat de l'index et du css

--- starterOnly/validation_de_champs.js
+++ starterOnly/validation_de_champs.js
@@ -1,23 +1,73 @@
 let form = document.querySelector('form');
 
-//Ecouter la modification de l'Email
-form.email.addEventListener('change', function() {
-    validEmail(this)
-});
-
 //Ecouter la modification du prénom
 form.first.addEventListener('change', function() {
     validFirst(this)
 });
 
+//Ecouter la modification du nom
+form.last.addEventListener('change', function() {
+    validLast(this)
+});
+
+//Ecouter la modification de l'Email
+form.email.addEventListener('change', function() {
+    validEmail(this)
+});
+
 //Ecouter la soumission du formulaire
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    if (validEmail(form.email) && validFirst(form.first)) {
+    if (validFirst(form.first) && validLast(form.last) && validEmail(form.email)) {
         displayModalSubmit();
         document.querySelector('form').reset();
         };
 });
+
+
+// VALIDATION Du Prénom
+const validFirst = function(inputFirst) {
+
+//Récupération du Paragraphe
+    let error = inputFirst.nextElementSibling;
+
+    if(/^[A-Za-z-]{2,20}$/.test(inputFirst.value)){
+        error.innerHTML = " Prénom Accepté";
+        error.classList.remove('text-danger');
+        error.classList.add('text-sucess');
+        first.style.border = 'solid green 0.19rem';
+        return true;
+    }
+    else {
+        error.innerHTML = "Veuillez entrer 2 caractères ou plus pour le Prénom et aucun chiffre";
+        error.classList.remove('text-sucess');
+        error.classList.add('text-danger');
+        first.style.border = 'solid red 0.19rem';
+        return false;
+    }
+};
+
+// VALIDATION Du Prénom
+const validLast = function(inputLast) {
+
+    //Récupération du Paragraphe
+        let error = inputLast.nextElementSibling;
+    
+        if(/^[A-Za-z-]{2,20}$/.test(inputLast.value)){
+            error.innerHTML = " Nom Accepté";
+            error.classList.remove('text-danger');
+            error.classList.add('text-sucess');
+            last.style.border = 'solid green 0.19rem';
+            return true;
+        }
+        else {
+            error.innerHTML = "Veuillez entrer 2 caractères ou plus pour le Nom et aucun chiffre";
+            error.classList.remove('text-sucess');
+            error.classList.add('text-danger');
+            last.style.border = 'solid red 0.19rem';
+            return false;
+        }
+    };
 
 // VALIDATION DE L'EMAIL
 const validEmail = function(inputEmail) {
@@ -39,27 +89,6 @@ const validEmail = function(inputEmail) {
         error.classList.remove('text-sucess');
         error.classList.add('text-danger');
         email.style.border = 'solid red 0.19rem';
-        return false;
-    }
-};
-
-// VALIDATION Du Prénom
-const validFirst = function(inputFirst) {
-
-    let error = inputFirst.nextElementSibling;
-
-    if(/^[A-Za-z-]{2,20}$/.test(inputFirst.value)){
-        error.innerHTML = " Prénom Accepté";
-        error.classList.remove('text-danger');
-        error.classList.add('text-sucess');
-        first.style.border = 'solid green 0.19rem';
-        return true;
-    }
-    else {
-        error.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom et aucun chiffre";
-        error.classList.remove('text-sucess');
-        error.classList.add('text-danger');
-        first.style.border = 'solid red 0.19rem';
         return false;
     }
 };
