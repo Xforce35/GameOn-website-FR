7ac976a4ff506453363dd455c0ee6c3aa5ee8da5
xforce35 <xforce35@hotmail.fr>
2021-11-29 09:42:36 +0100

    mise en beauté

--- starterOnly/js/validation_de_champs.js
+++ starterOnly/js/validation_de_champs.js
@@ -46,6 +46,7 @@ function validateFisrt() {
         showError(firstName);
         return false;
     }
+    //récupération du message d'erreur et le rendre invisible, ainsi que son style
     hideError(firstName);
     return true;
 };
@@ -58,38 +59,48 @@ function validateLast() {
         showError(lastName);
         return false;
     }
+    //récupération du message d'erreur et le rendre invisible, ainsi que son style
     hideError(lastName);
     return true;
 };
 
 //VALIDATION DE L'EMAIL
 function validateEmail() {
+    //On créé une constante avec une nouvelle expression régulière
     const EmailRegExp = new RegExp('^[a-zA-Z0-9._-]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+    //Si la valeur de l'input email est vide alors ça retourne faux
     if (email.value =="") {
         showError(email);
         return false; 
     }
+    //On teste l'exprésion régulière, si ça marche, ça retourne vrai
     if (EmailRegExp.test(email.value)) {
         hideError(email);
         return true;
     }
+    //si ça marche pas, ça retourne faux
    showError(email);
     return false;
 };
 
 //VALIDATION DE LA DATE DE NAISSANCE
 function validateBirthdate() {
+    //On créé une constante avec la valeur que l'utilisateur a rentré dans l'input birthdate
     const dtDOB = new Date(birthdate.value);
+    //On créé une constante avec la valeur de la date du jour
     const dtCurrent = new Date();
-    
+    // on vérifie qu'il y est 10 caractère d'entrer sinon ça retourne faux
     if (birthdate.value.trim().length !== 10) {
         showError(birthdate);
         return false; 
     }
+    // ici, on vérifie si la date d'anniversaire de l'utilisateur et celle du jour est inférieur à 18 ans et ça retourne faux
     if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
         showError(birthdate);
         return false;
-    } if (dtCurrent.getFullYear() - dtDOB.getFullYear() > 18) {
+    } 
+    // ici, on vérifie si la date d'anniversaire de l'utilisateur et celle du jour est supérieur à 18 ans et ça retourne vrai
+    if (dtCurrent.getFullYear() - dtDOB.getFullYear() > 18) {
         hideError(birthdate);
         return true;
     }
@@ -97,30 +108,22 @@ function validateBirthdate() {
 
 //VALIDATION DU NOMBRE DE TOURNOIS
 function validateQuantity() {
+    //Si la valeur de l'input quantity(nombre de tournois participer par l'utilisateur) est vide alors ça retourne faux
     if(quantity.value == "") {
         showError(quantity);
         return false;
         }
+    //On teste la différence avec cette nouvelle regex qui accepte que des chiffres de 0 a 9, une fois a deux fois chacun, si différent retourne faux
     if(!quantity.value.match(/^[0-9]{1,2}$/)) {
         showError(quantity);
         return false;
         }
+    //Sinon retourne vrai
     hideError(quantity);
     return true;
 };
 
 //VALIDATION DE LA VILLE
-//let isCytiSelected = false;
-//cities.forEach(city =>
-//{
-    //city.addEventListener('click', function (e)
-    //{
-        //isCytiSelected = true;
-        //hideError(cities);
-    //})
-    //showError(cities);
-        //return false;
-//});
 
 function validateCity() {
     allLocations.setAttribute('data-error-visible', 'true');
@@ -135,10 +138,12 @@ function validateCity() {
 
 //VALIDATION DES CONDITIONS G2N2RALES
 function validategenCondUse() {
+//Ici, on vérifie que la case des conditions générales d'utulisation soit soit coché donc checké, ce qui retourne vrai
 if (genCondUse.checked === false){
     showError(genCondUse);
     return false;
 }
+//Si ce n'est pas le cas, ça retourne faux
 hideError(genCondUse);
 return true;
 }
